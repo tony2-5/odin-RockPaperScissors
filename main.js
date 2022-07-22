b92eb00f2a4d9d564f5dbb496e27360016c6f70c
tony2-5 <antdvory@gmail.com>
2022-07-22 17:52:54 -0400

    Added win/loss mechanic
    First to 5 wins, added a game message and a new game button.

--- main.js
+++ main.js
@@ -2,6 +2,15 @@
 var playerScore = 0;
 var botScore = 0;
 
+newGameEl = document.getElementById("newGame");
+function resetScore() {
+    document.getElementById("gameMsg").innerHTML = "Choose wisely!"
+    playerScore = 0;
+    botScore = 0;
+    document.getElementById('scorePlayer').innerHTML = playerScore;
+    document.getElementById('scoreBot').innerHTML = botScore;
+}
+newGameEl.addEventListener('click', resetScore);
 
 let botChoice = () => {
     let randomInt = Math.floor((Math.random()*3)+1);
@@ -23,51 +32,72 @@ let botChoice = () => {
 
 const rockEl = document.getElementById('rockButton');
 let rockClicked = () => {
-    let opponentChoice = botChoice();
-    document.getElementById("vsImg1").src = "./images/rock-icon-8519.png";
-    switch(opponentChoice) {
-        case 'rock': null;
-        break;
-        case 'paper': botScore++;
-        break;
-        case 'scissors': playerScore++;
-        break;
+    if(playerScore < 5 && botScore < 5) {
+        let opponentChoice = botChoice();
+        document.getElementById("vsImg1").src = "./images/rock-icon-8519.png";
+        switch(opponentChoice) {
+            case 'rock': null;
+            break;
+            case 'paper': botScore++;
+            break;
+            case 'scissors': playerScore++;
+            break;
+        }
+        document.getElementById('scorePlayer').innerHTML = playerScore;
+        document.getElementById('scoreBot').innerHTML = botScore;
+    } else if(playerScore == 5) {
+        document.getElementById("gameMsg").innerHTML = "Congrats, you win! Select 'New Game' to play again."
+    } else if(botScore == 5) {
+        document.getElementById("gameMsg").innerHTML = "You lost :( select 'New Game' to try again."
     }
-    document.getElementById('scorePlayer').innerHTML = playerScore;
-    document.getElementById('scoreBot').innerHTML = botScore;
 }
 rockEl.addEventListener('click', rockClicked);
 
 const paperEl = document.getElementById('paperButton');
 let paperClicked = () => {
-    let opponentChoice = botChoice();
-    document.getElementById("vsImg1").src = "./images/paper-48284.png";
-    switch(opponentChoice) {
-        case 'rock': playerScore++;
-        break;
-        case 'paper': null;
-        break;
-        case 'scissors': botScore++;
-        break;
+    if(playerScore < 5 && botScore < 5) {
+        let opponentChoice = botChoice();
+        document.getElementById("vsImg1").src = "./images/paper-48284.png";
+        switch(opponentChoice) {
+            case 'rock': playerScore++;
+            break;
+            case 'paper': null;
+            break;
+            case 'scissors': botScore++;
+            break;
+        }
+        document.getElementById('scorePlayer').innerHTML = playerScore;
+        document.getElementById('scoreBot').innerHTML = botScore;
+    } else if(playerScore == 5) {
+        document.getElementById("gameMsg").innerHTML = "Congrats, you win! Select 'New Game' to play again."
+    } else if(botScore == 5) {
+        document.getElementById("gameMsg").innerHTML = "You lost :( select 'New Game' to try again."
     }
-    document.getElementById('scorePlayer').innerHTML = playerScore;
-    document.getElementById('scoreBot').innerHTML = botScore;
 }
+
 paperEl.addEventListener('click', paperClicked);
 
 const scissorsEl = document.getElementById('scissorsButton');
 let scissorsClicked = () => {
-    let opponentChoice = botChoice();
-    document.getElementById("vsImg1").src = "./images/scissors-icon-25521.png";
-    switch(opponentChoice) {
-        case 'rock': botScore++;
-        break;
-        case 'paper': playerScore++;
-        break;
-        case 'scissors': null;
-        break;
+    if(playerScore < 5 && botScore < 5) {
+        let opponentChoice = botChoice();
+        document.getElementById("vsImg1").src = "./images/scissors-icon-25521.png";
+        switch(opponentChoice) {
+            case 'rock': botScore++;
+            break;
+            case 'paper': playerScore++;
+            break;
+            case 'scissors': null;
+            break;
+        }
+        document.getElementById('scorePlayer').innerHTML = playerScore;
+        document.getElementById('scoreBot').innerHTML = botScore;
+    } else if(playerScore == 5) {
+        document.getElementById("gameMsg").innerHTML = "Congrats, you win! Select 'New Game' to play again."
+    } else if(botScore == 5) {
+        document.getElementById("gameMsg").innerHTML = "You lost :( select 'New Game' to try again."
     }
-    document.getElementById('scorePlayer').innerHTML = playerScore;
-    document.getElementById('scoreBot').innerHTML = botScore;
 }
 scissorsEl.addEventListener('click', scissorsClicked);
+
+
